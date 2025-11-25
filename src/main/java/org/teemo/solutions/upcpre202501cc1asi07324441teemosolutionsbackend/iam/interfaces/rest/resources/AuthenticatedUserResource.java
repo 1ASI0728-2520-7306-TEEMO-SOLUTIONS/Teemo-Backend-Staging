7f475f5712d631c9ea55602f6eeede1e88a3ad02
JoseRiega <u202211254@upc.edu.pe>
2025-11-25 12:32:48 -0500

    feat: enhance AuthenticatedUserResource with roles and add report endpoints to API

--- src/main/java/org/teemo/solutions/upcpre202501cc1asi07324441teemosolutionsbackend/iam/interfaces/rest/resources/AuthenticatedUserResource.java
+++ src/main/java/org/teemo/solutions/upcpre202501cc1asi07324441teemosolutionsbackend/iam/interfaces/rest/resources/AuthenticatedUserResource.java
@@ -1,4 +1,9 @@
 package org.teemo.solutions.upcpre202501cc1asi07324441teemosolutionsbackend.iam.interfaces.rest.resources;
 
-public record AuthenticatedUserResource(String id, String username, String token) {
+import java.util.List;
+
+public record AuthenticatedUserResource(String id,
+                                        String username,
+                                        String token,
+                                        List<String> roles) {
 }
--- src/main/java/org/teemo/solutions/upcpre202501cc1asi07324441teemosolutionsbackend/iam/interfaces/rest/transform/AuthenticatedUserResourceFromEntityAssembler.java
+++ src/main/java/org/teemo/solutions/upcpre202501cc1asi07324441teemosolutionsbackend/iam/interfaces/rest/transform/AuthenticatedUserResourceFromEntityAssembler.java
@@ -1,11 +1,19 @@
 package org.teemo.solutions.upcpre202501cc1asi07324441teemosolutionsbackend.iam.interfaces.rest.transform;
 
 import org.teemo.solutions.upcpre202501cc1asi07324441teemosolutionsbackend.iam.domain.model.aggregates.User;
+import org.teemo.solutions.upcpre202501cc1asi07324441teemosolutionsbackend.iam.domain.model.entities.Role;
 import org.teemo.solutions.upcpre202501cc1asi07324441teemosolutionsbackend.iam.interfaces.rest.resources.AuthenticatedUserResource;
+
+import java.util.Collection;
+import java.util.List;
 
 public class AuthenticatedUserResourceFromEntityAssembler {
     public static AuthenticatedUserResource toResourceFromEntity(User user,
                                                                  String token) {
-        return new AuthenticatedUserResource(user.getId(), user.getUsername(), token);
+        Collection<Role> assignedRoles = user.getRoles() != null ? user.getRoles() : List.of();
+        List<String> roles = assignedRoles.stream()
+                .map(Role::getStringName)
+                .toList();
+        return new AuthenticatedUserResource(user.getId(), user.getUsername(), token, roles);
     }
 }
